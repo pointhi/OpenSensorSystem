3bd83d3c4876d910034bb3693023640ee161c7ac
thomas <thomas@thomas-W150ER>
2013-08-24 11:00:45 +0200

    adding function to parse speed-definitions

---
+++ include/OssSpecialFunctions.hpp
@@ -0,0 +1,24 @@
+/**
+ * @file OssSpecialFunctions.hpp
+ *
+ * @author thomas
+ *
+ * @date 24. August 2013
+ * @copyright GNU General Public License (GPL) 3.0
+ */
+
+#ifndef OSSSPECIALFUNCTIONS_HPP
+#define	OSSSPECIALFUNCTIONS_HPP
+
+#include <string>
+
+namespace oss {
+    namespace specialFunctions {
+        namespace io {
+            double GetClockSpeedFromString(const std::string Clock);
+        }
+    }
+}
+
+#endif	/* OSSSPECIALFUNCTIONS_HPP */
+
---
+++ src/OssSpecialFunctions.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file OssSpecialFunctions.cpp
+ *
+ * @author thomas
+ *
+ * @date 24. August 2013
+ * @copyright GNU General Public License (GPL) 3.0
+ */
+
+#include <string>
+#include <sstream>
+
+#include "../include/OssSpecialFunctions.hpp"
+
+namespace oss {
+    namespace specialFunctions {
+        namespace io {
+
+            double GetClockSpeedFromString(const std::string clock) {
+
+                int i = 0;
+                for (; (isdigit(clock.at(i)) || ispunct(clock.at(i))) && i < clock.length(); i++);
+
+                std::string suffix = clock.substr(i, clock.length() - i);
+
+                std::stringstream ss;
+                ss << std::hex << clock.substr(0, i);
+                double clockSpeed = 0;
+                ss >> clockSpeed;
+
+                if (suffix == "Hz" || suffix == "") {
+                } else if (suffix == "kHz") {
+                    clockSpeed *= 1e3;
+                } else if (suffix == "MHz") {
+                    clockSpeed *= 1e6;
+                } else if (suffix == "GHz") {
+                    clockSpeed *= 1e9;
+                } else {
+                    clockSpeed = 0;
+                }
+
+                return clockSpeed;
+            }
+        }
+    }
+}
+
