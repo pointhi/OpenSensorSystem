d9ca9f33126f5f4b21fc24347931f4e64fe7f4fc
Thomas Pointhuber <thomas.pointhuber@gmx.at>
2013-09-14 11:25:09 +0200

    Fixed #1, implementing error-handling to root node and file-reading, edits on xml-stuctures

--- README.md
+++ README.md
@@ -27,37 +27,38 @@ std::cout << SensorSystem.DrawTree();   // Print Tree-Structure of Data
 
 ##Example XML-File
 
-Warning: at the current state, <?xml... wouldn't work
-also, the structure isn't finished design
+Warning: The structure isn't finished design
 
 ```XML
-<xml version="1.0" encoding="UTF-8" standalone="yes"/>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 
-<i2c name="I2C-0" device="/dev/i2c-0" clock="100kHz" status="disabled">
+<root>
+    <i2c name="I2C-0" device="/dev/i2c-0" clock="100kHz" status="disabled">
 
-</i2c>
+    </i2c>
 
-<i2c name="I2C-1" device="/dev/i2c-1" clock="100kHz">
-    <i2c-slave name="DIG0_PCF8574A" adress="0x38" maxclock="100kHz" interrupt="yes">
-        <lua-script file="lua/i2c/PCF8574.lua">
-            <sensor name="BMP_FRONT_LEFT" variable="b0"/>
-            <sensor name="BMP_FRONT_MIDDLE" variable="b1"/>
-            <sensor name="BMP_FRONT_RIGHT" variable="b2"/>
-        </lua-script>
-    </i2c-slave>
+    <i2c name="I2C-1" device="/dev/i2c-1" clock="100kHz">
+        <i2c-slave name="DIG0_PCF8574A" adress="0x38" maxclock="100kHz" interrupt="yes">
+            <lua-script file="lua/i2c/PCF8574.lua">
+                <sensor name="BMP_FRONT_LEFT" variable="b0"/>
+                <sensor name="BMP_FRONT_MIDDLE" variable="b1"/>
+                <sensor name="BMP_FRONT_RIGHT" variable="b2"/>
+            </lua-script>
+        </i2c-slave>
 
-    <i2c-slave name="DIG1_PCF8574A" adress="0x3A" maxclock="100kHz" interrupt="yes">
-        <lua-script file="lua/i2c/PCF8574.lua">
-            <sensor name="LINE_LEFT" variable="b0"/>
-        </lua-script>
-    </i2c-slave>
+        <i2c-slave name="DIG1_PCF8574A" adress="0x3A" maxclock="100kHz" interrupt="yes">
+            <lua-script file="lua/i2c/PCF8574.lua">
+                <sensor name="LINE_LEFT" variable="b0"/>
+            </lua-script>
+        </i2c-slave>
 
-    <smb-slave name="TMP1_TPA81" adress="0xD0" maxclock="100kHz" interrupt="no">
-        <lua-script file="lua/i2c/TPA81.lua">
-            <sensor name="ENVIROMENTAL_TEMP" variable="env_temp"/>
-            <sensor name="TMP_CAM_0" variable="tmp_cam_0"/>
-            <sensor name="TMP_CAM_1" variable="tmp_cam_1"/>
-        </lua-script>
-    </smb-slave>
-</i2c>
+        <smb-slave name="TMP1_TPA81" adress="0xD0" maxclock="100kHz" interrupt="no">
+            <lua-script file="lua/i2c/TPA81.lua">
+                <sensor name="ENVIROMENTAL_TEMP" variable="env_temp"/>
+                <sensor name="TMP_CAM_0" variable="tmp_cam_0"/>
+                <sensor name="TMP_CAM_1" variable="tmp_cam_1"/>
+            </lua-script>
+        </smb-slave>
+    </i2c>
+</root>
 ```
--- src/OssRootNode.cpp
+++ src/OssRootNode.cpp
@@ -34,34 +34,64 @@ namespace oss {
 
         std::string childName;
 
-        for (tinyxml2::XMLNode* HelpNode = xmlNode
-                ; HelpNode != NULL
-                ; HelpNode = HelpNode->NextSibling()
-                ) {
+        if (xmlNode) {
 
-            childName.clear();
-            childName = HelpNode->ToElement()->Name();
+            for (tinyxml2::XMLNode* HelpNode = xmlNode->ToElement()->FirstChild()
+                    ; HelpNode != NULL
+                    ; HelpNode = HelpNode->NextSibling()
+                    ) {
 
-            if (childName == "io") {
-            } else if (childName == "i2c") {
-                std::tr1::shared_ptr<oss::HostGroup> newChildElement(new oss::i2c::Host);
-                this->AddChildNode(newChildElement);
-                newChildElement->parseXml(HelpNode);
-            } else if (childName == "can") {
-            } else if (childName == "spi") {
-            } else if (childName == "uart") {
-            } else if (childName == "ethernet") {
+                childName.clear();
+                childName = HelpNode->ToElement()->Name();
+
+                if (childName == "io") {
+                } else if (childName == "i2c") {
+                    std::tr1::shared_ptr<oss::HostGroup> newChildElement(new oss::i2c::Host);
+                    this->AddChildNode(newChildElement);
+                    newChildElement->parseXml(HelpNode);
+                } else if (childName == "can") {
+                } else if (childName == "spi") {
+                } else if (childName == "uart") {
+                } else if (childName == "ethernet") {
+                }
             }
-        }
 
-        this->MainTreeGroup::parseMainXmlParameter(xmlNode);
+            this->MainTreeGroup::parseMainXmlParameter(xmlNode);
+        } else {
+            std::cerr << "ERROR: Node doesn't contain a <root> element!" << std::endl;
+        }
     }
 
     void RootNode::parseXml(std::string filePath) {
         tinyxml2::XMLDocument doc;
-        doc.LoadFile(filePath.c_str());
 
-        this->parseXml(doc.FirstChild());
+        switch (doc.LoadFile(filePath.c_str())) {
+            case tinyxml2::XML_NO_ERROR:
+                tinyxml2::XMLNode* HelpNode = doc.FirstChildElement("root");
+                if (HelpNode) {
+                    for (
+                            ; HelpNode != NULL
+                            ; HelpNode = HelpNode->NextSibling()
+                            ) {
+                        this->parseXml(HelpNode);
+                    }
+                } else {
+                    std::cerr << "ERROR: XML-File doesn't contain a <root> element!" << std::endl;
+                }
+                break;
+            case tinyxml2::XML_ERROR_FILE_NOT_FOUND:
+                std::cerr << "ERROR: File: \"" << filePath << "\" doesn't found" << std::endl;
+                break;
+            case tinyxml2::XML_ERROR_FILE_COULD_NOT_BE_OPENED:
+                std::cerr << "ERROR: File: \"" << filePath << "\" could not be open" << std::endl;
+                break;
+            case tinyxml2::XML_ERROR_EMPTY_DOCUMENT:
+                std::cerr << "ERROR: File: \"" << filePath << "\" doesn't contain data" << std::endl;
+                break;
+            default:
+                std::cerr << "ERROR: File: \"" << filePath << "\" throw unknow error" << std::endl;
+                break;
+        }
     }
 
 }
